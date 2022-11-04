f2d6f2583920ea12b33733216eabaa08178e716e
soyeon <luora@naver.com>
2022-11-04 20:19:52 +0900

    [add] BinaryTestTest

---
+++ soyeon/binarySearchTree/BinaryTestTest.java
@@ -0,0 +1,17 @@
+public class BinaryTestTest {
+	public static void main(String[] args) {
+		BinaryTree tree = new BinaryTree();
+
+		tree.insertNode(3);
+		tree.insertNode(2);
+		tree.insertNode(1);
+
+		if (tree.removeNode(2)) {
+			System.out.println("노드 삭제");
+		}
+
+		 tree.inorderTree(tree.rootNode, 0);
+		// tree.postorderTree(tree.rootNode, 0);
+		//tree.preorderTree(tree.rootNode, 0);
+	}
+}
